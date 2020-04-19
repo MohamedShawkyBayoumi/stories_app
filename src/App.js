398dfe3c574f55d2f4a1bdf84f2e19f812b77212
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-04-20 01:52:14 +0200

    handle routes and install packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <BrowserRouter>
+        <Route path='/' exact component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
