45b9e8ca64a1b0da1d7ff2e7eaa9cc821962dabe
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-04-20 17:02:48 +0200

    add signup page UI

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TextInput } from '../components/UI';
+import { Link } from 'react-router-dom';
 
 function Signup() {
+    const [isLoading, setIsLoading] = useState(false);
     return (
-        <div>
-            Signup
+        <div className="login-signup-page">
+            <div className="login-signup-page-content">
+                {isLoading && (
+                    <div className="loader-container">
+                        <div className="loader" />
+                    </div>
+                )}
+                <h1>Create new account</h1>
+                <form>
+                    <TextInput
+                        type="text"
+                        name="fullname"
+                        required
+                        onChange={() => console.log('test')}
+                        label="Full Name"
+                    />
+                    <TextInput
+                        type="text"
+                        name="job"
+                        required
+                        onChange={() => console.log('test')}
+                        label="Job Title"
+                    />
+                    <TextInput
+                        type="text"
+                        name="email"
+                        required
+                        onChange={() => console.log('test')}
+                        label="Email Address"
+                    />
+                    <TextInput
+                        type="password"
+                        name="password"
+                        required
+                        onChange={() => console.log('test')}
+                        label="Password"
+                    />
+                    <button type="submit">Create New Account</button>
+                </form>
+                <p className="own-account-p">Already have an account? <Link to='login'>Login</Link></p>
+            </div>
         </div>
     )
 }
 
-export default Signup
+export default Signup;
