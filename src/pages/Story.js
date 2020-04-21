b8d3d34c2d1940afd75038af7e922a0c186e978c
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-04-21 18:03:34 +0200

    add heart icon for add to favourites

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from '../components/Header'
 import Comment from '../components/Comment'
+import heart from '../assets/heart-icon.png'
+import heartOutline from '../assets/heart-icon-outline.png';
 
 function Story() {
     return (
         <div style={{ height: '100%' }}>
             <Header />
             <div className="single-story">
-                <h1>The standard Lorem Ipsum passage, used since the 1500s</h1>
+                <div className="head-title-heart">
+                    <h1>The standard Lorem Ipsum passage, used since the 1500s The standard Lorem Ipsum passage, used since the 1500s</h1>
+                    <div className="heart">
+                        <img src={heartOutline} alt="heart icon" />
+                    </div>
+                </div>
                 <p>
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                 </p>
