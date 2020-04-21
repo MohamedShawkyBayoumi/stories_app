a321c3ca0eb2db706d79fce9142210cb4fe1a8bf
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-04-21 17:41:53 +0200

    modify home UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import Header from '../components/Header'
+import StoryCard from '../components/UI/StoryCard'
 
 function Home() {
     return (
-        <div>
+        <div style={{ height: '100%' }}>
             <Header />
-            <div>
+            <div className="home">
                 <section className="main-cards">
                     <div className="main-card">
-                        <h3>Lorem ipsum</h3>
+                        <div>
+                            <h3>I have </h3>
+                            <h3>20 stories</h3>
+                        </div>
                     </div>
                     <div className="main-card">
-                        <h3>Lorem ipsum</h3>
+                        <div>
+                            <h3>I have </h3>
+                            <h3>30 comments</h3>
+                        </div>
                     </div>
                 </section>
+                <div className="home-stories">
+                    <StoryCard />
+                    <StoryCard />
+                    <StoryCard />
+                    <StoryCard />
+                    <StoryCard />
+
+                </div>
             </div>
         </div>
     )
